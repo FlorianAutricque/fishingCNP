30ef19f4cc9234673f44490d84712b701e179490
Florian Autricque <fautricque@gmail.com>
2026-05-13 10:55:19 +0200

    add page

--- fishingCNP/src/App.jsx
+++ fishingCNP/src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom'
 import { LoginPage } from './components/ms-login'
 import { DashboardPage } from './dashboard/DashboardPage.jsx'
+import { AwarenessScreen } from './components/ms-login/AwarenessScreen.jsx'
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<LoginPage />} />
       <Route path="/dashboard" element={<DashboardPage />} />
+      <Route path="/sensibilisation" element={<AwarenessScreen />} />
     </Routes>
   )
 }
